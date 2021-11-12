eedc162cbd1fa25c9a43710179225c849ce015d4
Daniel Iovescu <iovescu.dani@gmail.com>
2021-11-13 00:31:45 +0200

    base wasm load

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import ActionBar from "./components/actions-bar/ActionBar";
 import ImageViewer from "./components/image-viewer/ImageViewer";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {selectIsLoading} from "./features/core/coreSlice";
 import LoadingSpinner from "./components/loading-spinner/LoadingSpinner";
+import {loadWasmExample} from "./features/counter/counterSlice";
 
 function App() {
+  const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
+
+  useEffect(() => {
+    dispatch(loadWasmExample());
+  }, [dispatch]);
 
   return (
     <div>
